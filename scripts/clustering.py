e1955b959e68044b3f263ceaa37489b22f795e53
Animesh <garg.animesh@gmail.com>
2015-08-23 14:42:35 -0700

    commit before pulling changes

--- scripts/clustering.py
+++ scripts/clustering.py
@@ -160,7 +160,7 @@ class MilestonesClustering():
 
 		print "Generated big_N"
 
-		gmm = mixture.GMM(n_components = self.n_components_cp, covariance_type='full')
+		gmm = mixture.GMM(n_components = self.n_components_cp, covariance_type='full', tol = 0.01)
 		gmm.fit(big_N)
 		Y = gmm.predict(big_N)
 
@@ -576,7 +576,7 @@ class MilestonesClustering():
 
 		self.generate_transition_features()
 
-		self.generate_change_points_1()
+		self.generate_change_points_2()
 
 		self.cluster_changepoints_level1()
 
@@ -759,9 +759,9 @@ if __name__ == "__main__":
 
 		# list_of_demonstrations = ["Needle_Passing_D001", "Needle_Passing_D002","Needle_Passing_D003", "Needle_Passing_D004", "Needle_Passing_D005"]
 
-		list_of_demonstrations = ['Suturing_E001', 'Suturing_E002','Suturing_E003', 'Suturing_E004', 'Suturing_E005']
+		# list_of_demonstrations = ['Suturing_E001', 'Suturing_E002','Suturing_E003', 'Suturing_E004', 'Suturing_E005']
 
-		# list_of_demonstrations = ["0100_01", "0100_02", "0100_03", "0100_04", "0100_05"]
+		list_of_demonstrations = ["0001_01", "0001_02", "0001_03", "0001_04", "0001_05"]
 
 		# list_of_demonstrations = ['Suturing_E001','Suturing_E002', 'Suturing_E003', 'Suturing_E004', 'Suturing_E005',
 		# 'Suturing_D001','Suturing_D002', 'Suturing_D003', 'Suturing_D004', 'Suturing_D005',
